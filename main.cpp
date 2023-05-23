96ec3e7f3e0cf00fffc32d5921f5549aa7bf62e1
seba-the-programmer <megaseba9@gmail.com>
2023-05-23 20:42:28 +0200

    Add copyright

--- main.cpp
+++ main.cpp
@@ -12,3 +12,22 @@ int main() {
 
   return 0;
 }
+/*You may not share, distribute, or reproduce in any way any copyrighted
+ * material, trademarks, or other proprietary information belonging to others
+ * without obtaining the prior written consent of the owner of such proprietary
+ * rights. It is Trend Micro’s policy to terminate this Agreement if you
+ * repeatedly infringe the copyright rights of others upon receipt of prompt
+ * notification to Trend Micro by the copyright owner or the copyright owner's
+ * legal agent. Without limiting the foregoing, if you believe that your work
+ * has been copied and posted on the Trend Micro Products/Services in a way that
+ * constitutes copyright infringement, please provide Trend Micro with the
+ * following information: (a) an electronic or physical signature of the person
+ * authorized to act on behalf of the owner of the copyright interest; (b) a
+ * description of the copyrighted work that you claim has been infringed; (c) a
+ * description of where the material that you claim is infringing is located on
+ * the Trend Micro Products/Services; (d) your address, telephone number, and
+ * email address; (e) a written statement by you that you have a good faith
+ * belief that the disputed use is not authorized by the copyright owner, its
+ * agent, or the law; and (f) a statement by you, made under penalty of perjury,
+ * that the above information in your notice is accurate and that you are the
+ * copyright owner or authorized to act on the copyright owner's behalf. */
